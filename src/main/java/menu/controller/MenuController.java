0d819045d28edd617e8bdcb4dc086fa204e418f4
kangrae <kangrae0501@gmail.com>
2025-12-13 16:14:31 +0900

    fix: 카테고리와 메뉴는 Enum에서 Repository로 관리하도록 변경

--- src/main/java/menu/controller/MenuController.java
+++ src/main/java/menu/controller/MenuController.java
@@ -1,9 +1,13 @@
 package menu.controller;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.time.DayOfWeek;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Supplier;
 import menu.domain.Coach;
+import menu.dto.MenuResult;
+import menu.dto.Recommend;
 import menu.view.InputView;
 
 public class MenuController {
@@ -17,6 +21,8 @@ public class MenuController {
     public void run() {
         List<Coach> coaches = readCoachesName();
         readRestrictions(coaches);
+
+        makeMenuResult();
     }
 
     private List<Coach> readCoachesName() {
@@ -34,6 +40,16 @@ public class MenuController {
                 String restrictions = inputView.readRestrictions();
                 return coach.addRestrictions(Arrays.stream(restrictions.split(",")).toList());
             });
+        }
+    }
+
+    private void makeMenuResult() {
+        MenuResult menuResult = new MenuResult();
+        for (int day = 1; day <= 5; day++) {
+            Recommend recommend = new Recommend();
+            Category category = Category.pick(Randoms.pickNumberInRange(1, 5));
+
+            menuResult.put(DayOfWeek.of(day), recommend);
         }
     }
 
---
+++ src/main/java/menu/repository/CategoryRepository.java
@@ -0,0 +1,22 @@
+package menu.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CategoryRepository {
+
+    private final List<String> categories = new ArrayList<>();
+
+    private CategoryRepository() {
+        categories.add("일식");
+        categories.add("한식");
+        categories.add("중식");
+        categories.add("아시안");
+        categories.add("양식");
+    }
+
+    public static CategoryRepository init() {
+        return new CategoryRepository();
+    }
+
+}
